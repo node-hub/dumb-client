2f699553ab63a831062193195e3e01b8c8fed4e1
Joseph <z@charmed.tech>
2019-04-17 06:26:18 -0700

    Update url

--- server-url.js
+++ server-url.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let SERVER_URL = 'https://afternoon-eyrie-23717.herokuapp.com';
+let SERVER_URL = 'https://shrouded-everglades-62939.herokuapp.com';
 
 if (process.env.NODE_ENV === 'development') {
   SERVER_URL = 'http://127.0.0.1:3000';
