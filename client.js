d9dbf06ef9a8396ea4cd3ad9146b35134af01fcd
Joseph <z@charmed.tech>
2019-04-16 20:29:09 -0700

    Invoke display method of returned payload

--- client.js
+++ client.js
@@ -3,28 +3,25 @@
 // Readline
 const rl = require('./readline-interface.js');
 
-
 // initial connect to chat server (hubbub)
 // Socket.io
 let SERVER_URL = 'https://afternoon-eyrie-23717.herokuapp.com';
 const io = require('socket.io-client');
 let socket = io.connect(SERVER_URL);
 
-
 // client send one thing, payload from readline
-rl.on('line', (line) => {
+rl.on('line', line => {
   // if(line === 'tic-tac-node'){
   //   socket.disconnect();
   //   socket = io.connect();
   // }
 
   //client needs an `exit` command that takes it back to chat server
-  if(line === 'exit'){
+  if (line === 'exit') {
     console.log('Goodbye');
     socket.disconnect();
     socket = io.connect('http://localhost:3000');
-  }
-  else {
+  } else {
     console.log(`Received: ${line}`);
     socket.emit('input', line);
   }
@@ -33,8 +30,5 @@ rl.on('line', (line) => {
 // client will accept an object from any ext app
 // print object
 socket.on('output', payload => {
-  console.log(payload);
+  payload.display();
 });
-
-
-
